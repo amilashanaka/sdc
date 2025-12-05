6c4e8d9daaec9d1a92c100c373c121cfd9bc5cb6
Don Gunasinha <dsa.amilashanaka@gmail.com>
2025-12-05 15:02:04 +0000

    update

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <title>Spicer Consulting Device Login</title>
 
   <!-- Bootstrap 5.3 (jsDelivr CDN) -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link href="./assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <!-- Font Awesome -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
   <!-- Google Fonts: Inter -->
