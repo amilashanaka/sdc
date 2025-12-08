af1d17a0972ce8e5c5eafc2870bcd350349bd8b9
Don Gunasinha <dsa.amilashanaka@gmail.com>
2025-12-08 15:34:33 +0000

    update

--- controllers/config.php
+++ controllers/config.php
@@ -9,9 +9,9 @@ define('DB_PASS', '');
 define('DB_NAME','sdc');
 
 
-// define('DB_USER','u710400283_tenx');
-// define('DB_PASS', 'Tenx@2024');
-// define('DB_NAME','u710400283_tenx');
+// define('DB_USER','root');
+// define('DB_PASS', 'daq');
+// define('DB_NAME','daq');
  
 
 // URI parameters
