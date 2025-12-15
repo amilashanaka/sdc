c82534afe8ae72df809dda4311ff1f3b6d1879a7
Don Gunasinha <dsa.amilashanaka@gmail.com>
2025-12-15 11:36:53 +0000

    Update install.sh

--- pynq/install.sh
+++ pynq/install.sh
@@ -18,8 +18,8 @@ set -euo pipefail
 
 LOG_FILE="/tmp/install_$(date +%Y%m%d_%H%M%S).log"
 APP_DIR="/var/www/html"
-WWW_USER="www-data"
-WWW_GROUP="www-data"
+WWW_USER="xilinx"
+WWW_GROUP="root"
 DB_PASS="daq"
 DB_NAME="daq"
 SERVER_IP="$(hostname -I | awk '{print $1}')"
